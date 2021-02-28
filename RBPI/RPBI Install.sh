eeeae89cf3fa825e3835f2cb09aa73ca2ab8daf3
Francesco Ferri <fcferri@outlook.com>
2021-02-27 20:03:11 -0800

    Initial Commit

---
+++ RBPI/RPBI Install.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# checking for errors
+mon_errors() {
+  if ! [ $? = 0 ]
+  then
+    echo "An error occured! Aborting...."
+    exit 1
+  fi
+}
+
+# begin
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Access Point Installation"
+read -p "To begin with the installation type in 'yes': " out
+if ! [ "$out" = "yes" ]
+then
+  echo "Exiting..."
+  exit 1
+fi
+
+# getting install paramenters from user
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+read -p "Enter the SSID: " my_ssid
+read -p "Enter the password: " my_pw
+
+if [ "${ap_ssid}" ] && [ "${ap_pass}" ]
+then
+    echo "SSID and password entered"
+else
+  echo "Empty SSID or password. Exiting..."
+  exit 1
+fi
+# showing available internet connections
+nmcli device status
+read -p "Enter the modem interface from above: " mod_interface
+echo "Starting..."
+
+# preparing for install
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "${mod_interface} down for installation"
+sudo ifconfig ${mod_interface} down
+mon_errors
+echo "Updating package sources..."
+sudo apt-get update -y
+sudo apt-get upgrade -y
+mon_errors
+echo "Installing hostapd dnsmasq"
+sudo apt-get install hostapd -y
+sudo apt-get install dnsmasq -y
+sudo apt-get install bridge-utils -y
+mon_errors
+echo "Stopping hostapd and dnsmasq down"
+sudo systemctl stop hostapd
+sudo systemctl stop dnsmasq
+mon_errors
+
+# Adding DHCP configuration
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Writing to /etc/dhcpcd.conf ..."
+text="
+interface wlan0
+static ip_address=0.0.0.0/24
+nohook wpa_supplicant
+"
+sudo sh -c "echo '${text}'>>/etc/dhcpcd.conf"
+sudo chmod 777 /etc/dhcpcd.conf
+mon_errors
+
+# DHCP Range config
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Configuring dnsmasq"
+echo "Writing to /etc/dnsmasq.conf ..."
+text="
+interface=wlan0
+dhcp-range=192.168.0.2,192.168.0.99,255.255.255.0,24h
+"
+sudo sh -c "echo '${text}'>/etc/dnsmasq.conf"
+sudo chmod 777 /etc/dnsmasq.conf
+mon_errors
+
+# WiFi Setup
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Configuring hostapd"
+echo "Writing to /etc/hostapd/hostapd.conf ..."
+text="
+interface=wlan0
+driver=nl80211
+#bridge=br0
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+ssid=${ap_ssid}
+wpa_passphrase=${ap_pass}
+"
+sudo sh -c "echo '${text}'>/etc/hostapd/hostapd.conf"
+sudo chmod 777 /etc/hostapd/hostapd.conf
+mon_errors
+echo "Writing to /etc/default/hostapd..."
+text="
+DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"
+"
+sudo sh -c "echo '${text}'>/etc/default/hostapd"
+sudo chmod 777 /etc/default/hostapd
+mon_errors
+
+# forwarding traffic from AP to active connection
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Installing traffic forwarding"
+echo "Appending to /etc/sysctl.conf..."
+text="
+net.ipv4.ip_forward=1
+"
+sudo sh -c "echo '${text}'>>/etc/sysctl.conf"
+mon_errors
+
+# updating IP tables
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "add iptables rule"
+sudo iptables -t nat -A POST -o ${mod_interface} -j MASQ
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+echo "Appending to /etc/rc.local..."
+text="
+iptables-restore < /etc/iptables.ipv4.nat
+exit 0
+"
+sudo sed -i '/exit 0/d'  /etc/rc.local
+sudo sh -c "echo '${text}'>>/etc/rc.local"
+sudo chown root:root /etc/rc.local
+sudo chmod 777 /etc/rc.local
+
+# Finishing up Installation
+echo "${mod_interface} is up"
+sudo ifconfig ${mod_interface} up
+mon_errors
+echo "hostapd and dnsmasq is up"
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
+mon_errors
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Installation is complete."
---
+++ Ubuntu Server/Serverbuild.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+set -x
+
+# checking for errors
+mon_errors() {
+  if ! [ $? = 0 ]
+  then
+    echo "An error occured! Aborting...."
+    exit 1
+  fi
+}
+
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Installing packages..."
+sudo apt-get -y update                            
+sudo apt-get -y upgrade
+sudo apt-get -y install screen
+sudo apt-get -y install tcptrack
+sudo apt-get -y install python 
+sudo apt-get -y install python-wxgtk2.8 
+sudo apt-get -y install python-matplotlib  
+sudo apt-get -y install python-opencv 
+sudo apt-get -y install python-numpy  
+sudo apt-get -y install python-dev 
+sudo apt-get -y install libxslt-dev
+sudo apt-get -y install python-lxml
+sudo apt-get -y install python-setuptools
+sudo apt-get -y install python3 
+sudo apt-get -y install python3-matplotlib                                                                                          
+sudo apt-get -y install python3-opencv                                                                                        
+sudo apt-get -y install python3-pip                                                                                            
+sudo apt-get -y install python3-numpy                                                                                         
+sudo apt-get -y install python3-dev                                                                                            
+sudo apt-get -y install git 
+sudo apt-get -y install dh-autoreconf
+sudo apt-get -y install systemd 
+sudo apt-get -y install wget 
+sudo apt-get -y install emacs                                            
+sudo apt-get -y install emacs                                              
+sudo apt-get -y install nload                                         
+sudo apt-get -y install build-essential 
+sudo apt-get -y install autossh 
+sudo pip install future 
+sudo pip install pymavlink 
+sudo pip install mavproxy 
+sudo pip3 install future                                                                                                       
+sudo pip3 install pymavlink                                                                                                
+sudo pip3 install mavproxy
+mon_errors
+
+
+# Downloading mavlink repo
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Downloading git clone"                                                                                      
+git clone https://github.com/intel/mavlink-router.git
+cd mavlink-router
+sudo git submodule update --init --recursive
+echo "Start making / compiling / building mavlink-router..."
+sudo ./autogen.sh && sudo ./configure CFLAGS='-g -O2' --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 --prefix=/usr --disable-systemd
+sudo make
+mon_errors
+
+
+#Configure mavlink router
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Configuration: mavlink data stream on localhost port 5678 TCP"                                                                                             
+if [ ! -d "/etc/mavlink-router" ] 
+then
+    echo "Directory /etc/mavlink-router does not exist. Making it." 
+    sudo mkdir /etc/mavlink-router
+    echo "Made /etc/mavlink-router" 
+fi
+cd /etc/mavlink-router
+sudo chmod 777 main.conf
+echo "Done configuring mavlink-router..."
+mon_errors
+
+
+# download mavlink start scripts
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Now download the autostart scripts for mavlink-router"
+if [ ! -d "/home/ubuntu/startupscripts" ] 
+then
+    echo "Directory /home/ubuntu/startupscripts does not exist yet. Making it." 
+    sudo -u ubuntu mkdir /home/ubuntu/startupscripts
+    echo "Made /home/ubuntu/startupscripts" 
+fi
+cd /home/ubuntu/startupscripts
+sudo chmod 777 /home/ubuntu/startupscripts/start_mavlinkrouter.sh
+sudo chmod 777 /home/ubuntu/startupscripts/autostart_mavlinkrouter.sh
+mon_errors
+
+
+# rc.local to be finished
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Downloading /etc/rc.local"
+# Need to add filepath with wget once done
+sudo chmod 777 /etc/rc.local
+echo "Downloaded /etc/rc.local"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+sudo chmod 777 /etc/ssh/sshd_config
+mon_errors
+
+
+echo "Installation Complete. Exiting..."
